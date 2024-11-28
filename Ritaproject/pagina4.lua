667320672c4aee1f743120d6684d4dc0bbb1239c
Rita Santos <110061623+GITH12RITA@users.noreply.github.com>
2024-11-28 09:39:53 -0300

    Update pagina4.lua

--- Ritaproject/pagina4.lua
+++ Ritaproject/pagina4.lua
@@ -12,22 +12,74 @@ function scene:create(event)
     background.y = display.contentCenterY
 
     -- Imagem do Homem
-    local homem = display.newImageRect(sceneGroup, "assets/homem.png", 340, 600) -- Substitua o caminho da imagem
+    local homem = display.newImageRect(sceneGroup, "assets/homem.png", 340, 600)
     homem.x = display.contentCenterX + 200
-    homem.y = display.contentCenterY -- Ajuste conforme necessário para centralizar a imagem
+    homem.y = display.contentCenterY
 
-    -- Botão Verde (Porta de Entrada)
-    local botaoVerde = display.newCircle(sceneGroup, display.contentCenterX +200, display.contentCenterY - 420, 40)
-    botaoVerde:setFillColor(0, 1, 0) -- Cor verde
+    -- Imagem do Homem
+        local ferida = display.newImageRect(sceneGroup, "assets/ferida.png", 45,45)
+        ferida.x = display.contentCenterX +273
+        ferida.y = display.contentCenterY -130
+
+        -- Imagem do Homem
+    local corte = display.newImageRect(sceneGroup, "assets/corte.png", 40, 45)
+    corte.x = display.contentCenterX +175
+    corte.y = display.contentCenterY+ 45
+
+    -- Grupos para pontos verdes e amarelos
+    local grupoVerde = display.newGroup()
+    local grupoAmarelo = display.newGroup()
+    sceneGroup:insert(grupoVerde)
+    sceneGroup:insert(grupoAmarelo)
+
+    -- Função para criar pontos
+    local function criarPonto(grupo, x, y, cor)
+        local ponto = display.newCircle(grupo, x, y, 10)
+        if cor == "verde" then
+            ponto:setFillColor(0, 45, 0) -- Cor verde
+        elseif cor == "amarelo" then
+            ponto:setFillColor(45, 45, 0) -- Cor amarela
+        end
+    end
+
+    -- Criando pontos verdes (portas de entrada)
+    criarPonto(grupoVerde, homem.x, homem.y - 200, "verde") --boca
+    criarPonto(grupoVerde, homem.x, homem.y - 220, "verde") -- Nariz
+    criarPonto(grupoVerde, homem.x -15, homem.y - 240, "verde") -- Olhos
+    criarPonto(grupoVerde, homem.x +20, homem.y - 240, "verde") -- Olhos
+    criarPonto(grupoVerde, homem.x, homem.y -80, "verde") -- Estômago
+    criarPonto(grupoVerde, homem.x, homem.y -10, "verde") -- Trato urinário
+    criarPonto(grupoVerde, homem.x -20, homem.y +40, "verde") -- Corte na perna
+    criarPonto(grupoVerde, homem.x + 80 , homem.y -120, "verde") -- braço
+
+
+    -- Criando pontos amarelos (portas de saída)
+    criarPonto(grupoAmarelo, homem.x, homem.y - 220, "amarelo") -- Nariz
+    criarPonto(grupoAmarelo, homem.x, homem.y - 200, "amarelo") -- boca
+    criarPonto(grupoAmarelo, homem.x-20, homem.y +40, "amarelo") -- corte na perna
+    criarPonto(grupoAmarelo, homem.x, homem.y - 10, "amarelo") -- Trato urinário
+    criarPonto(grupoAmarelo, homem.x + 80, homem.y - 120, "amarelo") -- braço
+
+    -- Inicialmente, ambos os grupos de pontos estão invisíveis
+    grupoVerde.isVisible = false
+    grupoAmarelo.isVisible = false
+
+    -- Botão Verde (Portas de Entrada)
+    local botaoVerde = display.newCircle(sceneGroup, display.contentCenterX + 200, display.contentCenterY - 420, 40)
+    botaoVerde:setFillColor(0, 45, 0) -- Cor verde
     botaoVerde:addEventListener("tap", function()
         print("Botão verde pressionado: Porta de entrada")
+        grupoVerde.isVisible = true
+        grupoAmarelo.isVisible = false
     end)
 
-    -- Botão Amarelo (Porta de Saída)
-    local botaoAmarelo = display.newCircle(sceneGroup, display.contentCenterX + 100, display.contentCenterY  -420, 40)
-    botaoAmarelo:setFillColor(1, 1, 0) -- Cor amarela
+    -- Botão Amarelo (Portas de Saída)
+    local botaoAmarelo = display.newCircle(sceneGroup, display.contentCenterX + 100, display.contentCenterY - 420, 40)
+    botaoAmarelo:setFillColor(45, 45, 0) -- Cor amarela
     botaoAmarelo:addEventListener("tap", function()
         print("Botão amarelo pressionado: Porta de saída")
+        grupoVerde.isVisible = false
+        grupoAmarelo.isVisible = true
     end)
 
     -- Botão Próximo
